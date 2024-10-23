7661e7360c53eecccc772ef337e54dbbaea8e40c
Eugene Volodin <Euvolodin2@yandex.ru>
2024-10-23 17:43:55 +0300

    Почти исходный вариант

--- src/main/java/org/example/repository/RoleRepository.java
+++ src/main/java/org/example/repository/RoleRepository.java
@@ -1,4 +1,7 @@
 package org.example.repository;
 
-public interface RoleRepository {
+import org.example.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
